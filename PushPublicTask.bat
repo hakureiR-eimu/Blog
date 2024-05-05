e856ae0e2f3c9310a5988c61e3769982ff500523
CodeOfUsami <zhang0285@foxmail.com>
2024-05-05 20:22:47 +0800

    feat:Update PushPublicTask.bat

--- PushPublicTask.bat
+++ PushPublicTask.bat
@@ -1,11 +1,13 @@
 @echo off
 
 if "%~1"=="" (
+    hugo
     echo Update:default
     git add -f public/
     git commit -m "Update:default"
     git push origin main
 ) else (
+    hugo
     echo %~1
     git add -f public/
     git commit -m %1
