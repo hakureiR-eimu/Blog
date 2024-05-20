f07a3a66ecbca1f8034a279e3662b695f8f26641
CodeOfUsami <zhang0285@foxmail.com>
2024-05-20 14:31:06 +0800

    feat:add archtypes/default

---
+++ utils/add_draft_to_files.ps1
@@ -0,0 +1,21 @@
+﻿
+
+# Write-Output "Hello, World世界!"
+
+if($args.length -ge 1){
+    $path = $args[0]
+}else {
+    $path = "./content/docs/日记"    
+}
+
+try {
+    if(Test-Path -Path $path -PathType Container){
+        Write-Output "Path $path is a directory"
+    }elseif(Test-Path -Path $path -PathType Leaf){
+        Write-Output "Path $path is a file"
+    }else{
+        throw "Invalid path: $path"
+    }
+}catch{
+    Write-Error $_.Exception.Message
+}
